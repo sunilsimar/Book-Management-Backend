a41ecfd70b1f9c9a8e08275b20eeb13965dc254f
Sunil <sunisimar2002d@gmail.com>
2023-11-06 15:34:42 +0530

    adding all the files

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
+// const { configDotenv } = require("dotenv");
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const bookRouter = require("./routes/book");
+require("dotenv").config();
 
 app.use(express.json());
+
+app.use("/", bookRouter);
+
+//connect to mongoose
+mongoose.connect(process.env.MONGODB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: "Backend",
+});
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
---
+++ server/routes/book.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const Book = require("../db/db");
+
+const router = express.Router();
+
+//Add a new book route
+router.post("/newbook", async (req, res) => {
+  const { title, author, summary } = req.body;
+
+  try {
+    if (!title || !author || !summary) {
+      return res.status(400).json({ message: "Incomplete book data" });
+    }
+
+    const book = new Book({ title, author, summary });
+    const savedBook = await book.save();
+    res.status(201).json({
+      message: "New book added successfully",
+      book: savedBook,
+      bookid: savedBook._id,
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Book not added", error: err });
+  }
+});
+
+//Update book
+router.put("/book/:bookId", async (req, res) => {
+  const book = await Book.findByIdAndUpdate(req.params.bookId, req.body, {
+    new: true,
+  }); //mongoose function
+  if (book) {
+    res.json({ message: "Book updated successfully" });
+  } else {
+    res.status(404).json({ message: "Book not found" });
+  }
+});
+
+//delete book
+router.delete("/deletebook/:bookId", async (req, res) => {
+  const bookId = req.params.bookId;
+
+  try {
+    const deletedBook = await Book.findByIdAndDelete(bookId);
+
+    if (!deletedBook) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    res.json({ message: "Book deleted successfully" });
+  } catch (err) {
+    console.error("Error deleting book:", err);
+    res
+      .status(500)
+      .json({ message: "Error deleting book", error: err.message });
+  }
+});
+
+//get books
+router.get("/books", async (req, res) => {
+  const books = await Book.find({});
+  res.json({ books });
+});
+
+//get specific book
+router.get("/book/:bookId", async (req, res) => {
+  const bookId = req.params.bookId;
+  const book = await Book.findById(bookId);
+  res.json({ book });
+});
+
+module.exports = router;
